e875df6d673849b04b953a906b0bb4e2112de357
akakou <akakou571@gmail.com>
2023-01-31 15:27:14 +0900

    use rustc-link-search instead of env file

--- build.rs
+++ build.rs
@@ -13,13 +13,9 @@ fn run_command(script: &str) {
         .output()
         .expect(&error_msg);
 
-    let stdout =
-        String::from_utf8(output.stdout.clone()).expect("error: encode command output to utf-8");
+    String::from_utf8(output.stdout.clone()).expect("error: encode command output to utf-8");
     let stderr =
         String::from_utf8(output.stderr.clone()).expect("error: encode command output to utf-8");
-
-    println!("stdout: {}", stdout);
-    println!("stderr: {}", stderr);
 
     if !output.status.success() {
         panic!("error: failed to running commands\n{}", stderr);
@@ -38,4 +34,5 @@ fn main() {
     }
 
     run_command(&build_scripts);
+    println!("cargo:rustc-link-search={MCL_FOLDER}/lib");
 }
