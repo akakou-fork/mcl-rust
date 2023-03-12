17e9a9913c2841ba6059543c5485468d54a4563e
akakou <akakou571@gmail.com>
2023-03-13 08:18:34 +0900

    [update] override old library

--- build.rs
+++ build.rs
@@ -4,14 +4,11 @@ fn main() {
     let url1 = "https://web.sfc.keio.ac.jp/~t19503ka/res/libmcl.a";
     let url2 = "https://web.sfc.keio.ac.jp/~t19503ka/res/libmclbn384_256.a";
 
-    let cmd = format!(
-        "mkdir -p build/ && cd build/ && wget {} {}",
-        url1, url2
-    );
+    let cmd = format!("mkdir -p build/ && cd build/ && wget -O {} {}", url1, url2);
     Command::new("sh")
         .args(["-c", &cmd])
         .output()
         .expect("fail");
-    
+
     println!("cargo:rustc-link-search=./build/");
 }
